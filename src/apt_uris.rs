67352961ee3edd700b0041a3d38ba91ced450533
Michael Aaron Murphy <mmstickman@gmail.com>
2019-01-08 13:09:27 -0700

    feat: Support parallel / asynchronous fetching of apt packages

---
+++ src/apt_uris.rs
@@ -0,0 +1,111 @@
+use async_fetcher::{AsyncFetcher, FetchError, FetchEvent};
+use command::Command;
+use reqwest::r#async::Client;
+use std::io;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::path::Path;
+use md5::Md5;
+use futures::{Future, IntoFuture};
+
+pub fn apt_uris() -> Result<Vec<AptUri>, AptUriError> {
+    let output = Command::new("apt-get")
+        .env("DEBIAN_FRONTEND", "noninteractive")
+        .args(&["--print-uris", "full-upgrade"])
+        .run_with_stdout()
+        .map_err(AptUriError::Command)?;
+
+    let mut packages = Vec::new();
+    for line in output.lines() {
+        if !line.starts_with('\'') {
+            continue
+        }
+
+        packages.push(line.parse::<AptUri>()?);
+    }
+
+    Ok(packages)
+}
+
+#[derive(Debug, Error)]
+pub enum AptUriError {
+    #[error(display = "apt command failed to execute: {}", _0)]
+    Command(io::Error),
+    #[error(display = "uri not found in output: {}", _0)]
+    UriNotFound(String),
+    #[error(display = "invalid URI value: {}", _0)]
+    UriInvalid(String),
+    #[error(display = "name not found in output: {}", _0)]
+    NameNotFound(String),
+    #[error(display = "size not found in output: {}", _0)]
+    SizeNotFound(String),
+    #[error(display = "size in output could not be parsed as an integer: {}", _0)]
+    SizeParse(String),
+    #[error(display = "md5sum not found in output: {}", _0)]
+    Md5NotFound(String),
+    #[error(display = "md5 prefix (MD5Sum:) not found in md5sum: {}", _0)]
+    Md5Prefix(String)
+}
+
+#[derive(Debug, Clone)]
+pub struct AptUri {
+    pub uri: String,
+    pub name: String,
+    pub size: u64,
+    pub md5sum: String
+}
+
+impl AptUri {
+    pub fn fetch(self, client: &Client) -> impl Future<Item = Self, Error = FetchError> {
+        const ARCHIVES: &str = "/var/cache/apt/archives/";
+        const PARTIAL: &str = "/var/cache/apt/archives/partial/";
+
+        let dest: Arc<Path> = Path::new(ARCHIVES).join(&self.name).into();
+        let part: Arc<Path> = Path::new(PARTIAL).join(&self.name).into();
+
+        let name = self.name.clone();
+
+        AsyncFetcher::new(client, self.uri.clone())
+            .with_progress_callback(move |event| match event {
+                FetchEvent::Get => println!("Getting {}", name),
+                FetchEvent::DownloadComplete
+                    | FetchEvent::AlreadyFetched => println!("Finished {}", name),
+                _ => ()
+            })
+            .request_to_path_with_checksum::<Md5>(dest, &self.md5sum.clone())
+            .then_download(part)
+            .then_rename()
+            .into_future()
+            .map(move |_| self)
+    }
+}
+
+impl FromStr for AptUri {
+    type Err = AptUriError;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut words = line.split_whitespace();
+
+        let mut uri = words.next().ok_or_else(|| AptUriError::UriNotFound(line.into()))?;
+
+        // We need to remove the single quotes that apt-get encloses the URI within.
+        if uri.len() <= 3 {
+            return Err(AptUriError::UriInvalid(uri.into()))
+        } else {
+            uri = &uri[1..uri.len() - 1];
+        }
+
+        let name = words.next().ok_or_else(|| AptUriError::NameNotFound(line.into()))?;
+        let size = words.next().ok_or_else(|| AptUriError::SizeNotFound(line.into()))?;
+        let size = size.parse::<u64>().map_err(|_| AptUriError::SizeParse(size.into()))?;
+        let mut md5sum = words.next().ok_or_else(|| AptUriError::Md5NotFound(line.into()))?;
+
+        if md5sum.starts_with("MD5Sum:") {
+            md5sum = &md5sum[7..];
+        } else {
+            return Err(AptUriError::Md5Prefix(md5sum.into()));
+        }
+
+        Ok(AptUri { uri: uri.into(), name: name.into(), size, md5sum: md5sum.into() })
+    }
+}
--- src/release/mod.rs
+++ src/release/mod.rs
@@ -1,5 +1,9 @@
 use clap::ArgMatches;
+use futures::{stream, Stream};
+use reqwest::r#async::Client;
+use tokio::runtime::Runtime;
 
+use ::apt_uris::{apt_uris, AptUri, AptUriError};
 use ::release_architecture::{detect_arch, ReleaseArchError};
 use ::release_version::{detect_version, ReleaseVersionError};
 use ::release_api::{ApiError, Release};
@@ -11,7 +15,9 @@ pub enum ReleaseError {
     #[error(display = "failed to fetch release architecture: {}", _0)]
     ReleaseArch(ReleaseArchError),
     #[error(display = "failed to fetch release versions: {}", _0)]
-    ReleaseVersion(ReleaseVersionError)
+    ReleaseVersion(ReleaseVersionError),
+    #[error(display = "failed to fetch apt URIs to fetch: {}", _0)]
+    AptList(AptUriError)
 }
 
 impl From<ReleaseVersionError> for ReleaseError {
@@ -29,6 +35,8 @@ impl From<ReleaseArchError> for ReleaseError {
 pub fn release(matches: &ArgMatches) -> Result<(), ReleaseError> {
     match matches.subcommand() {
         ("check", _) => check_release()?,
+        ("fetch", Some(matches)) => fetch_packages(matches)?,
+        ("upgrade", Some(matches)) => do_upgrade(matches)?,
         _ => unimplemented!()
     }
 
@@ -39,11 +47,30 @@ fn check_release() -> RelResult<()> {
     let (current, next) = detect_version()?;
 
     println!(
-        "     Current Release: {}\n         Next Release: {}\nNew Release Available: {}",
+        "      Current Release: {}\n         Next Release: {}\nNew Release Available: {}",
         current,
         next,
         Release::get_release(&next, "intel").is_ok()
     );
 
+    Ok(())
+}
+
+fn fetch_packages(matches: &ArgMatches) -> Result<(), ReleaseError> {
+    let client = Client::new();
+
+    let stream_of_downloads = stream::iter_ok(apt_uris().map_err(ReleaseError::AptList)?);
+
+    let buffered_stream = stream_of_downloads
+        .map(move |uri| uri.fetch(&client))
+        .buffer_unordered(8);
+
+    let results = Runtime::new().unwrap().block_on(buffered_stream.collect());
+    println!("completed: {:#?}", results);
+
+    Ok(())
+}
+
+fn do_upgrade(matches: &ArgMatches) -> Result<(), ReleaseError> {
     Ok(())
 }
