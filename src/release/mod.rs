b5f3b7c0eedbe1e8f766a12d76b2a2e740947fda
Michael Aaron Murphy <mmstickman@gmail.com>
2019-01-09 11:27:12 -0700

    refactor: Revert apt release changes on error / Migrate apt-uris to apt-fetcher

--- src/release/mod.rs
+++ src/release/mod.rs
@@ -1,13 +1,14 @@
-use apt_fetcher::{DistUpgradeError, UpgradeRequest, SourcesList};
+use apt_fetcher::{DistUpgradeError, UpgradeRequest, Upgrader, SourcesList};
+use apt_fetcher::apt_uris::{apt_uris, AptUri, AptUriError};
 use apt_keyring::AptKeyring;
+use async_fetcher::FetchError;
 use clap::ArgMatches;
-use futures::{stream, Stream};
+use futures::{stream, Future, Stream};
 use reqwest::r#async::Client;
 use tokio::runtime::Runtime;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-use ::apt_uris::{apt_uris, AptUri, AptUriError};
 use ::release_architecture::{detect_arch, ReleaseArchError};
 use ::release_version::{detect_version, ReleaseVersionError};
 use ::release_api::{ApiError, Release};
@@ -25,7 +26,12 @@ pub enum ReleaseError {
     #[error(display = "unable to upgrade to next release: {}", _0)]
     Check(DistUpgradeError),
     #[error(display = "failure to overwrite release files: {}", _0)]
-    Overwrite(DistUpgradeError)
+    Overwrite(DistUpgradeError),
+    #[error(display = "root is required for this action: rerun with `sudo`")]
+    NotRoot,
+    #[error(display = "fetch of package failed: {}", _0)]
+    PackageFetch(FetchError)
+
 }
 
 impl From<ReleaseVersionError> for ReleaseError {
@@ -40,10 +46,9 @@ impl From<ReleaseArchError> for ReleaseError {
     }
 }
 
-pub fn release(matches: &ArgMatches) -> Result<(), ReleaseError> {
+pub fn release(matches: &ArgMatches) -> RelResult<()> {
     match matches.subcommand() {
         ("check", _) => check_release()?,
-        ("fetch", Some(matches)) => fetch_packages(matches)?,
         ("upgrade", Some(matches)) => do_upgrade(matches)?,
         _ => unimplemented!()
     }
@@ -99,7 +104,7 @@ impl UbuntuCodename {
     }
 }
 
-fn fetch_packages(matches: &ArgMatches) -> Result<(), ReleaseError> {
+fn do_upgrade(matches: &ArgMatches) -> RelResult<()> {
     let client = Arc::new(
         Client::builder()
             .timeout(Duration::from_secs(3))
@@ -108,43 +113,63 @@ fn fetch_packages(matches: &ArgMatches) -> Result<(), ReleaseError> {
     );
 
     let (current, next) = detect_version()?;
-    release_upgrade(client.clone(), &current, &next)?;
+    let mut upgrader = release_upgrade(client.clone(), &current, &next)?;
 
-    let stream_of_downloads = stream::iter_ok(apt_uris().map_err(ReleaseError::AptList)?);
+    fn attempt_fetch(client: Arc<Client>) -> RelResult<()> {
+        let apt_uris = apt_uris(&["full-upgrade"]).map_err(ReleaseError::AptList)?;
+        let size: u64 = apt_uris.iter().map(|v| v.size).sum();
 
-    let buffered_stream = stream_of_downloads
-        .map(move |uri| uri.fetch(&client))
-        .buffer_unordered(8);
+        println!("Fetching {} packages ({} MiB total)", apt_uris.len(), size / 1024 / 1024);
 
-    let results = Runtime::new().unwrap().block_on(buffered_stream.collect());
-    println!("completed: {:#?}", results);
+        let stream_of_downloads = stream::iter_ok(apt_uris);
+        let buffered_stream = stream_of_downloads
+            .map(move |uri| uri.fetch(&client))
+            .buffer_unordered(8)
+            .for_each(|v| Ok(()))
+            .map_err(ReleaseError::PackageFetch);
 
-    Ok(())
-}
+        Runtime::new().unwrap().block_on(buffered_stream)
+    }
 
-fn do_upgrade(matches: &ArgMatches) -> Result<(), ReleaseError> {
-    let (current, next) = detect_version()?;
-    // release_upgrade(&current, &next)?;
+    match attempt_fetch(client) {
+        Ok(_) => println!("packages fetched successfully"),
+        Err(why) => {
+            eprintln!("failed to fetch packages: {}", why);
+            eprintln!("rolling back apt release files");
+            if let Err(why) = upgrader.revert_apt_sources() {
+                eprintln!("failed to revert release name changes to source lists in /etc/apt/");
+            }
+            ::std::process::exit(1);
+        }
+    }
 
     Ok(())
 }
 
 /// Check if release files can be upgraded, and then overwrite them with the new release.
-fn release_upgrade(client: Arc<Client>, current: &str, new: &str) -> Result<(), ReleaseError> {
+fn release_upgrade(client: Arc<Client>, current: &str, new: &str) -> Result<Upgrader, ReleaseError> {
     let current = UbuntuCodename::from_version(current).map_or(current, |c| c.as_codename());
     let new = UbuntuCodename::from_version(new).map_or(new, |c| c.as_codename());
 
     let sources = SourcesList::scan().unwrap();
     let client = Arc::new(Client::new());
-    let keyring = Arc::new(AptKeyring::new().unwrap());
 
     println!("Checking if release can be upgraded from {} to {}", current, new);
     let mut upgrade = UpgradeRequest::new(client, sources)
-        .keyring(keyring)
         .send(current, new)
         .map_err(ReleaseError::Check)?;
 
     println!("Upgrade is possible -- updating release files");
     upgrade.overwrite_apt_sources()
-        .map_err(ReleaseError::Overwrite)
+        .map_err(ReleaseError::Overwrite)?;
+
+    Ok(upgrade)
+}
+
+fn check_root() -> RelResult<()> {
+    if unsafe { libc::geteuid() } != 0 {
+        Err(ReleaseError::NotRoot)
+    } else {
+        Ok(())
+    }
 }
