9924d6bbe9cd3af17ac45add544f5eaf7f210cbf
Michael Aaron Murphy <mmstickman@gmail.com>
2019-02-25 15:42:47 -0700

    :hammer: Update the pop-upgrade script

--- data/pop-upgrade.sh
+++ data/pop-upgrade.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-echo "performing system upgrade"
-if apt full-upgrade -y; then
+message () {
+    echo "$2"
+    test "$1" = "-i" && {
+        plymouth message --text="$2"
+    }
+    test "$1" = "-f" && {
+        plymouth update --status="failed"
+        plymouth message --text="$2"
+        plymouth update --status="normal"
+    }
+}
+
+message -i "performing system upgrade"
+if apt full-upgrade -y --allow-downgrades; then
     rm /pop-upgrade /system-update
-    echo "upgrade complete, rebooting the system"
+    message -i "upgrade complete, rebooting the system"
     systemctl reboot
 else
-    echo "upgrade failed; dropping to a shell for recovery:"
+    message -f "upgrade failed; dropping to a shell for recovery:"
     echo "  * type `reboot` and enter to restart the machine."
     echo "  * type `journalctl pop-upgrade-script` to step through upgrade logs"
     echo "  * System76 customers may create support tickets for help"
