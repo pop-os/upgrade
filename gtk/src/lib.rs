eb6a0c8bb2155d1f6b88ac5e299746097b387dd0
Michael Aaron Murphy <mmstick@pm.me>
2019-09-06 16:57:13 -0600

    feat(gtk): Display desktop notification when release upgrade is finished downloading

--- gtk/src/lib.rs
+++ gtk/src/lib.rs
@@ -7,6 +7,7 @@ extern crate log;
 #[macro_use]
 extern crate shrinkwraprs;
 
+mod notify;
 mod widgets;
 
 use self::widgets::*;
@@ -187,6 +188,17 @@ impl UpgradeWidget {
                     }
                     UiEvent::CompleteDownload => {
                         upgrade_downloaded = true;
+
+                        let version = upgrading_to.clone();
+                        thread::spawn(move || {
+                            notify::notify(
+                                "distributor-logo-upgrade-symbolic",
+                                &format!("Pop!_OS {} is ready to upgrade", version),
+                                "Click here to restart",
+                                || reboot(),
+                            );
+                        });
+
                         option_upgrade
                             .button_view()
                             .button_label("Upgrade")
