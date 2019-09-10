4a93c0d967c99a7de49f2299a9356be117ac3e5e
Michael Aaron Murphy <mmstick@pm.me>
2019-09-10 15:36:45 -0600

    improvement(cli): Improved colors of events and their messages

--- src/cli/colors.rs
+++ src/cli/colors.rs
@@ -7,17 +7,17 @@ pub(crate) fn color_error_desc<T: ::std::fmt::Display>(value: T) -> Paint<T> {
 }
 
 pub(crate) fn color_info<T: ::std::fmt::Display>(value: T) -> Paint<T> {
-    Paint::yellow(value).bold()
-}
-
-pub(crate) fn color_primary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
     Paint::green(value).bold()
 }
 
-pub(crate) fn color_secondary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
+pub(crate) fn color_primary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
     Paint::cyan(value).bold()
 }
 
-pub(crate) fn color_tertiary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
+pub(crate) fn color_secondary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
     Paint::blue(value).bold()
+}
+
+pub(crate) fn color_tertiary<T: ::std::fmt::Display>(value: T) -> Paint<T> {
+    Paint::magenta(value).bold()
 }
