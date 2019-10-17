c17d7e6701a51c7e917d6321585b75fe3155eb6b
Michael Aaron Murphy <mmstick@pm.me>
2019-10-17 13:31:59 -0600

    feat(core): Support fetching changelogs since a release

---
+++ src/changelogs.rs
@@ -0,0 +1,10 @@
+use std::cmp::Ordering;
+
+pub const CHANGELOGS: &[(&str, &str)] = &[("19.10", include_str!("../changelogs/19.10"))];
+
+pub fn since<'a>(release: &'a str) -> impl Iterator<Item = (&'static str, &'static str)> + 'a {
+    CHANGELOGS
+        .into_iter()
+        .filter(move |(version, _)| human_sort::compare(release, *version) == Ordering::Less)
+        .map(|(version, changelog)| (*version, *changelog))
+}
