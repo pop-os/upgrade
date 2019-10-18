0df0439a288173e8093f3253383bb7ab526c7807
Michael Aaron Murphy <mmstick@pm.me>
2019-10-18 14:23:51 -0600

    fix: Notifications now use distributor-logo icon

--- src/notify.rs
+++ src/notify.rs
@@ -2,7 +2,7 @@ use notify_rust::{Notification, Timeout};
 
 pub fn notify<F: FnOnce()>(next: &str, func: F) {
     Notification::new()
-        .icon("distributor-logo-upgrade-symbolic")
+        .icon("distributor-logo")
         .summary("Upgrade Available")
         .body(&["Pop!_OS ", next, " is available to download"].concat())
         .action("default", "default")
