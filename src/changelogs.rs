408c4ccab35f5a9a113a9e3819be8c25cc7f1b70
shpurk <benpack101@gmail.com>
2019-11-12 08:01:58 -0700

    feat(changelogs): Add initial changelogs for 19.04 and 18.10

--- src/changelogs.rs
+++ src/changelogs.rs
@@ -1,6 +1,10 @@
 use std::cmp::Ordering;
 
-pub const CHANGELOGS: &[(&str, &str)] = &[("19.10", include_str!("../changelogs/19.10"))];
+pub const CHANGELOGS: &[(&str, &str)] = &[
+    ("19.10", include_str!("../changelogs/19.10")),
+    ("19.04", include_str!("../changelogs/19.04")),
+    ("18.10", include_str!("../changelogs/18.10")),
+];
 
 pub fn since<'a>(release: &'a str) -> impl Iterator<Item = (&'static str, &'static str)> + 'a {
     CHANGELOGS
