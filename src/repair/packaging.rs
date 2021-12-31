9a22de24ca18c08223d5ad26dd5c992f7c55569f
Michael Aaron Murphy <mmstick@pm.me>
2021-12-31 18:43:04 +0100

    feat(repair): On package repair, ensure libc6 and other problematic packages have their candidates installed

--- src/repair/packaging.rs
+++ src/repair/packaging.rs
@@ -1,6 +1,7 @@
 use crate::release::repos::{iter_files, PPA_DIR};
 use anyhow::Context;
 use apt_cmd::{lock::apt_lock_wait, AptGet, Dpkg};
+use futures::StreamExt;
 use std::fs;
 use ubuntu_version::Codename;
 
@@ -43,7 +44,7 @@ pub async fn repair(release: &str) -> anyhow::Result<()> {
             .await
             .context("failed to configure packages with `dpkg --configure -a`");
 
-        last_error = a.and(b);
+        last_error = a.and(b).and(base_requirements().await);
 
         if last_error.is_ok() {
             break;
@@ -51,4 +52,45 @@ pub async fn repair(release: &str) -> anyhow::Result<()> {
     }
 
     last_error
+}
+
+const PROBLEMATIC_PACKAGES: &[&str] = &[
+    "zlib1g",
+    "libc6",
+    "libc6:i386",
+    "ppp",
+    "libnm0",
+    "libc++1",
+    "libc++1:i386",
+    "libmount1:i386",
+];
+
+async fn base_requirements() -> anyhow::Result<()> {
+    info!("ensuring prerequisites are installed");
+
+    // Fetch apt-cache policies for each of the problematic packages.
+    let (mut child, policies) = apt_cmd::AptCache::new().policy(PROBLEMATIC_PACKAGES).await?;
+
+    // Remember which packages are installed with candidates.
+    let mut to_install = Vec::new();
+
+    futures_util::pin_mut!(policies);
+
+    while let Some(policy) = policies.next().await {
+        if policy.installed != "(none)" && policy.installed != policy.candidate {
+            to_install.push(format!("{}={}", policy.package, policy.candidate));
+        }
+    }
+
+    let _ = child.status();
+
+    info!("installing required prerequisites: {:?}", to_install);
+
+    // Ensure that the packages have their candidate versions installed.
+    AptGet::new()
+        .allow_downgrades()
+        .noninteractive()
+        .install(to_install)
+        .await
+        .context("failed to install prerequisites")
 }
