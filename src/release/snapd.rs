24f23291adee5958560d5f3dbc29161912fdfc80
Michael Aaron Murphy <mmstick@pm.me>
2022-02-11 20:57:02 +0100

    fix: Do not error if transitional snap packages not found

--- src/release/snapd.rs
+++ src/release/snapd.rs
@@ -8,22 +8,21 @@ use tokio::fs;
 /// and before packages have been fetched.
 pub async fn hold_transitional_packages() -> Result<(), ReleaseError> {
     let mut out = String::new();
-    let snap_packages = AptCache::predepends_of(&mut out, "snapd")
-        .await
-        .map_err(ReleaseError::TransitionalSnapFetch)?;
 
-    let mut buffer = String::new();
+    if let Ok(snap_packages) = AptCache::predepends_of(&mut out, "snapd").await {
+        let mut buffer = String::new();
 
-    for package in &snap_packages {
-        buffer.push_str(*package);
-        buffer.push('\n');
+        for package in &snap_packages {
+            buffer.push_str(*package);
+            buffer.push('\n');
+        }
+
+        fs::write(crate::TRANSITIONAL_SNAPS, buffer.as_bytes())
+            .await
+            .map_err(ReleaseError::TransitionalSnapRecord)?;
+
+        let _ = AptMark::new().hold(&snap_packages).await;
     }
-
-    fs::write(crate::TRANSITIONAL_SNAPS, buffer.as_bytes())
-        .await
-        .map_err(ReleaseError::TransitionalSnapRecord)?;
-
-    let _ = AptMark::new().hold(&snap_packages).await;
 
     Ok(())
 }
