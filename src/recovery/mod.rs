6c3b0abc6fa702e06d68fc0c58c80662015ef068
Michael Aaron Murphy <mmstick@pm.me>
2022-02-23 22:56:58 +0100

    chore: Change recovery download max part size to 16 MB

--- src/recovery/mod.rs
+++ src/recovery/mod.rs
@@ -279,7 +279,7 @@ async fn from_remote(
         Fetcher::default()
             .timeout(std::time::Duration::from_secs(5))
             .connections_per_file(4)
-            .max_part_size(4 * 1024 * 1024)
+            .max_part_size(16 * 1024 * 1024)
             .events(events_tx)
             .shutdown(cancel)
             .build()
