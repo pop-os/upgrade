d9c08b1527b9f0d05aa52140fb95a13a1450701e
Michael Aaron Murphy <mmstick@pm.me>
2022-04-11 15:26:25 +0200

    fix: Changelog path

--- daemon/src/changelogs.rs
+++ daemon/src/changelogs.rs
@@ -1,14 +1,14 @@
 use std::cmp::Ordering;
 
 pub const CHANGELOGS: &[(&str, &str)] = &[
-    ("22.04", include_str!("../changelogs/22.04")),
-    ("21.10", include_str!("../changelogs/21.10")),
-    ("21.04", include_str!("../changelogs/21.04")),
-    ("20.10", include_str!("../changelogs/20.10")),
-    ("20.04", include_str!("../changelogs/20.04")),
-    ("19.10", include_str!("../changelogs/19.10")),
-    ("19.04", include_str!("../changelogs/19.04")),
-    ("18.10", include_str!("../changelogs/18.10")),
+    ("22.04", include_str!("../../changelogs/22.04")),
+    ("21.10", include_str!("../../changelogs/21.10")),
+    ("21.04", include_str!("../../changelogs/21.04")),
+    ("20.10", include_str!("../../changelogs/20.10")),
+    ("20.04", include_str!("../../changelogs/20.04")),
+    ("19.10", include_str!("../../changelogs/19.10")),
+    ("19.04", include_str!("../../changelogs/19.04")),
+    ("18.10", include_str!("../../changelogs/18.10")),
 ];
 
 pub fn since<'a>(release: &'a str) -> impl Iterator<Item = (&'static str, &'static str)> + 'a {
