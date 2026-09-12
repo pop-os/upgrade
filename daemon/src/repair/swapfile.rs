edcd1e70f5df4a14f4b273fd91f4f5ebcfa7f5d3
Michael Aaron Murphy <michael@mmurphy.dev>
2026-09-12 02:02:31 +0200

    fix: more robust detection of available disk capacity for swapfile

--- daemon/src/repair/swapfile.rs
+++ daemon/src/repair/swapfile.rs
@@ -4,25 +4,28 @@ use rustix::fs::FallocateFlags;
 use std::{os::unix::fs::OpenOptionsExt, path::Path, process::Command};
 
 const SWAPFILE_PATH: &str = "/swapfile";
+const MEBIBYTE: u64 = 1_048_576;
 
 pub fn create() -> Result<(), Error> {
     let fs_stats = rustix::fs::statfs("/").map_err(Error::RootFsStats)?;
-    let blocks_per_mib = u64::try_from(1_048_576 / fs_stats.f_bsize)
-        .map_err(|_| Error::RootBlockSizeInvalid { size: fs_stats.f_bsize })?;
-    let available_mib = fs_stats.f_bavail / blocks_per_mib;
+    let Ok(block_size) = u64::try_from(fs_stats.f_bsize) else {
+        return Err(Error::RootBlockSizeInvalid { size: fs_stats.f_bsize })?;
+    };
+
+    let available_mib = fs_stats.f_bavail * block_size / MEBIBYTE;
 
     // Up to 40% of total physical memory.
     let swapfile_capacity = {
         let mut sysinfo = sysinfo::System::new();
         sysinfo.refresh_memory();
-        let max_swapfile = sysinfo.total_memory() / 1_048_576 * 10 / 25;
+        let max_swapfile = sysinfo.total_memory() / MEBIBYTE * 10 / 25;
         let disk_limit = if available_mib < 20480 {
             return Ok(());
         } else {
             available_mib - 20480
         };
 
-        max_swapfile.min(disk_limit).max(4096) * 1_048_576
+        max_swapfile.min(disk_limit).max(4096) * MEBIBYTE
     };
 
     if let Ok(existing_swapfile) = std::fs::metadata(SWAPFILE_PATH) {
